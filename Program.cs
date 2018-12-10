387017785f51ddb9e0ce467a7802d4cd39c2609a
Rohizzle <Rohizzle@users.noreply.github.com>
2018-12-10 12:57:58 +0000

    dateTime code

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myDate = DateTime.Now;
+            string formattedDate = "";
+            formattedDate = string.Format("{0:dd-MM-yyyy HH:mm:ss}",myDate);
+            Console.WriteLine(formattedDate);
+            
+        }
+    }
+}
