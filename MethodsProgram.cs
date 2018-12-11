f9a8544eeb9597e7178c62c598faaaca684913ea
Rohizzle <Rohizzle@users.noreply.github.com>
2018-12-11 15:15:34 +0000

    Methods

---
+++ MethodsProgram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            int addResult = 0;
+            a = 5;
+            b = 3;
+            addResult = PerformAddOperation(a, b);
+             Console.WriteLine($"{a} +{b} = {addResult}");
+             DisplayMessage();
+            //  PerformAddOperation(a, b);
+
+            c = 15;
+            d = 10;
+            addResult = PerformAddOperation(c, d);
+            Console.WriteLine($"{c} +{d} = {addResult}");
+            DisplayMessage();
+            //PerformAddOperation(c, d);
+
+        }
+        static int PerformAddOperation(int x, int y)
+        {
+            int addResult = 0;
+            addResult = x + y;
+            //Console.WriteLine($"{x} +{y} = {addResult}");
+            //DisplayMessage();
+            return addResult;
+
+        }
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Process is done");
+            Console.WriteLine("This process is run by Roheel");
+            Console.WriteLine("Finished on time : "+DateTime.Now.ToShortTimeString());
+        }
+    }
+}
