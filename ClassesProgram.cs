7dc2e95cb8e9e27914156d50b685a68942129b67
Rohizzle <Rohizzle@users.noreply.github.com>
2018-12-11 15:14:58 +0000

    Classes

---
+++ ClassesProgram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person perOne = new Person();
+            perOne.FirstName = "Roheel";
+            perOne.LastName = "Arshad";
+            perOne.Country = "Scotland";
+
+            Person perTwo = new Person();
+            perTwo.FirstName = "Mohfizzle";
+            perTwo.LastName = "Foshizzle";
+            perTwo.Country = "UK";
+
+            Console.WriteLine(perOne.FirstName);
+            Console.WriteLine(perTwo.FirstName);
+        }
+    }
+
+    class Person
+    {
+        string firstName;
+        string lastName;
+        DateTime birthDate;
+        string country;
+
+        public string FirstName
+        {
+            get { return firstName;  }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value;  }
+        }
+
+        public DateTime BirthDate { get; set; }
+        public string Country { get; set; }
+    }
+}
